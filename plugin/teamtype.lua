c25863528e9b56c626c6b2660d5bd4590857d6e4
alerque <caleb@alerque.com>
2026-06-03 12:14:17 +0000

    Automatic mirror of commit 5e82deb in main Teamtype repository

--- plugin/teamtype.lua
+++ plugin/teamtype.lua
@@ -1,12 +1,17 @@
 -- SPDX-FileCopyrightText: 2025 blinry <mail@blinry.org>
 -- SPDX-FileCopyrightText: 2025 zormit <nt4u@kpvn.de>
+-- SPDX-FileCopyrightText: 2026 Caleb Maclennan <caleb@alerque.com>
 --
 -- SPDX-License-Identifier: AGPL-3.0-or-later
 
 local teamtype = require("teamtype")
 
+-- Enable testing or using a teamtype binary outside of the path without
+-- creating a dedicated config file for it. Used in e2e testing.
+local binary_name = os.getenv("TEAMTYPE_BINARY") or "teamtype"
+
 teamtype.config("teamtype", {
-    cmd = { "teamtype", "client" },
+    cmd = { binary_name, "client" },
     root_markers = ".teamtype",
 })
 
